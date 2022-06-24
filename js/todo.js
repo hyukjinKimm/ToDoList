aaca401716f917a9c857ecc2340bce82dc59d3ef
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-06-24 14:37:03 +0900

    feat: Edit todo.js to include Saving todos features

--- js/todo.js
+++ js/todo.js
@@ -1,25 +1,48 @@
 const todoForm = document.querySelector('#todo-form');
 const todoInput = document.querySelector('#todo-form input');
 const todoList = document.querySelector('#checklist');
+const TODO_KEY = 'todos';
+let todos = [];
 
 function submitTodo(e){
   e.preventDefault();
-  const todo = todoInput.value;
-  paintingTodo(todo);
+  const newtodo = {
+    text: todoInput.value,
+    id: Date.now(),
+  };
+
+  paintingTodo(newtodo);
+  saveTodo(newtodo);
+  todoInput.value = '';
+
 }
 
 function paintingTodo(todo){
   const input = document.createElement('input');
   const label = document.createElement('label');
 
-  const id =String(Date.now());
-  input.id = id
   input.type = 'checkbox'
   input.name = 'r';
-  label.htmlFor = id;
-  label.innerText = todo;
+  input.id = todo.id
+
+  label.htmlFor = todo.id;
+  label.innerText = todo.text;
   todoList.appendChild(input);
   todoList.appendChild(label);
-  todoInput.value = '';
 }
+function saveTodo(todo){
+
+  todos.push(todo);
+  localStorage.setItem(TODO_KEY, JSON.stringify(todos));
+}
+
 todoForm.addEventListener('submit', submitTodo);
+
+const savedTodo = localStorage.getItem(TODO_KEY);
+
+if(savedTodo){
+  todos = JSON.parse(savedTodo);
+  todos.forEach(paintingTodo);
+} else {
+
+}
